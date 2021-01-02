df4233afc25cefbd3fb07d5d92d74b1172169025
Andrew DePompa <addepompa@gmail.com>
2021-01-02 10:39:52 -0500

    fix: pull port and base path from env vars

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	phttp "github.com/toast2e/preservationnc-server/http"
 )
 
 func main() {
-	http.HandleFunc("/properties", phttp.GetAllPropertiesHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("SERVER_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	route := os.Getenv("BASE_PATH")
+	if route == "" {
+		route = "/preservationnc"
+	}
+
+	http.HandleFunc(fmt.Sprintf("%s/properties", route), phttp.GetAllPropertiesHandler)
+	log.Printf("Server started on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
