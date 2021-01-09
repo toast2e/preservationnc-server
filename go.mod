9eab956c17a2e53f6e7bb8a3c632ecfbb5b4e3a3
Andrew DePompa <addepompa@gmail.com>
2021-01-09 08:26:15 -0500

    feat: adds mongodb to docker-compose
    - adds mongo driver to go.mod
    - sets up sample db config
    - inserts sample db data

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/toast2e/preservationnc-server
 
 go 1.15
+
+require go.mongodb.org/mongo-driver v1.4.4
--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	phttp "github.com/toast2e/preservationnc-server/http"
+	"github.com/toast2e/preservationnc-server/reps"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	ctx := context.Background()
+	ctx = setupDB(ctx)
+	defer shutdown(ctx)
+
 	port := os.Getenv("SERVER_PORT")
 	if port == "" {
 		port = "8080"
@@ -23,4 +31,47 @@ func main() {
 	http.HandleFunc(fmt.Sprintf("%s/properties", route), phttp.GetAllPropertiesHandler)
 	log.Printf("Server started on port %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func setupDB(ctx context.Context) context.Context {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://db:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := client.Database("preservationnc")
+	properties := db.Collection("properties")
+	res, err := properties.InsertOne(ctx, reps.Property{
+		Name:        "raleighProperty1",
+		Description: "testDescription",
+		Price:       200000.00,
+		Location: reps.Site{
+			Address: reps.Address{
+				Number: "123",
+				Street: "Fake Street",
+			},
+			City:      "Raleigh",
+			State:     "North Carolina",
+			Zip:       "12345",
+			Latitude:  35.8436867,
+			Longitude: -78.7851406,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID = %s\n", res.InsertedID)
+
+	return context.WithValue(ctx, "mongodb:client", client)
+}
+
+func shutdown(ctx context.Context) {
+	mongoClient, ok := ctx.Value("mongodb:client").(mongo.Client)
+	if ok {
+		mongoClient.Disconnect(ctx)
+	}
 }
